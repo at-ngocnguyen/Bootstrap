76751bc685e85f4014ce5af4edb120bacc85c92f
at-ngocnguyen <ngoc.nguyen@asiantech.vn>
2020-05-19 09:01:08 +0700

    upload badge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ButtonComponent } from './feature/button/button.component';
+import { FormsComponent } from './feature/forms/forms.component';
+import { BadgeComponent } from './feature/badge/badge.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'button', component: ButtonComponent },
+  { path: 'forms', component: FormsComponent },
+  { path: 'badge', component: BadgeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './feature/button/button.component';
+import { FormsComponent } from './feature/forms/forms.component';
+import { BadgeComponent } from './feature/badge/badge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    FormsComponent,
+    BadgeComponent
   ],
   imports: [
     BrowserModule,
